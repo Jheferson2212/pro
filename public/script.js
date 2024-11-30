dfd777ac36372b37d1627fec9c5e55fd9564ede0
Jheferson2212 <104408232+Jheferson2212@users.noreply.github.com>
2024-11-30 03:16:41 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Función para formatear un número con punto de miles
     function formatNumberWithCommas(number) {
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     }
 
+    // Función para calcular el dinero a recibir
     function calcularDineroRecibir() {
         let montoEnviar = parseFloat(document.getElementById("fieldname2_3").value.replace(/\./g, '').replace(',', '.'));
         let tipoCambio = parseFloat(document.getElementById("fieldname3_3").value);
@@ -12,9 +14,36 @@ document.addEventListener("DOMContentLoaded", function() {
         let monedaRecibir = "";
 
         if (isNaN(montoEnviar)) {
-            montoEnviar = 0;
+            montoEnviar = 0; // Establecer monto a 0 si es NaN
         }
 
         if (selectedOption.includes("Colombia ► Argentina")) {
             monedaEnviar = "COP";
             monedaRecibir = "ARS";
+        } else if (selectedOption.includes("Argentina ► Colombia")) {
+            monedaEnviar = "ARS";
+            monedaRecibir = "COP";
+        }
+
+        const dineroRecibir = Math.round(montoEnviar * tipoCambio);
+        document.getElementById("fieldname2_3").value = formatNumberWithCommas(montoEnviar);
+        document.getElementById("moneda_enviar_3").textContent = monedaEnviar;
+        document.getElementById("fieldname6_3").value = formatNumberWithCommas(dineroRecibir); // Redondear a número entero
+        document.getElementById("moneda_recibir_3").textContent = monedaRecibir;
+    }
+
+    // Evento input para el campo "Dinero a Enviar"
+    document.getElementById("fieldname2_3").addEventListener("input", function() {
+        if (this.value === "") {
+            this.value = "0"; // Establecer el valor a 0 si está vacío
+        }
+        this.value = formatNumberWithCommas(parseFloat(this.value.replace(/\./g, '').replace(',', '.'))) || "";
+        calcularDineroRecibir();
+    });
+
+    // Evento change para el campo "Tipo de cambio"
+    document.getElementById("fieldname3_3").addEventListener("change", calcularDineroRecibir);
+
+    // Llamar a la función calcularDineroRecibir al cargar la página
+    calcularDineroRecibir();
+});
